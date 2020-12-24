59eeeb7d491d4c6e46ac99042f6a35a89e70e430
kamaldaniels <kamaldaniels@gmail.com>
2020-12-24 21:19:23 +0000

    Add scss. Remove nuxt logo. Update favicon. Add page description.

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">Kamal Daniels</h1>
-      <div class="links">
-        <a
-          href="https://github.com/kamaldaniels"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <Logo />
+    <h1 class="title">Kamal Daniels</h1>
+    <div class="links">
+      <a
+        href="https://github.com/kamaldaniels"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="button"
+      >
+        GitHub
+      </a>
     </div>
   </div>
 </template>
@@ -21,35 +19,35 @@
 export default {}
 </script>
 
-<style>
+<style lang="scss" scoped>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  grid-template: repeat(3, 1fr) / 1fr;
   text-align: center;
+  min-height: 100vh;
+  align-items: center;
 }
 
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
   font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
+  font-size: 5rem;
+  color: $text;
   letter-spacing: 1px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.button {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
 }
 
-.links {
-  padding-top: 15px;
+.button:hover {
+  color: #fff;
+  background-color: #3b8070;
 }
 </style>
