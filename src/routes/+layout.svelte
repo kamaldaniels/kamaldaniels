a63f94ef43bfd1eb135e7cb6041db55c304c04c2
Kamal Daniels <kamal.daniels@nanoporetech.com>
2023-02-04 14:07:22 +0000

    Add new v2 of site using svelte-kit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<svelte:head>
+	<link href="https://fonts.googleapis.com/css?family=Raleway:wght" rel="stylesheet" />
+</svelte:head>
+
+<div class="app">
+	<Header />
+	<main><slot /></main>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem 2rem;
+		width: 100%;
+		max-width: 60rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	@media (min-width: 720px) {
+		main {
+			padding: 1rem 3rem;
+		}
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About</h1>
+	<p>
+		Welcome to my personal website, where I explore and experiment with the latest technology and
+		software engineering trends. This site is a creative outlet for me to showcase my skills, share
+		my thoughts and insights, and most importantly, have fun!
+	</p>
+	<p>
+		I believe that software engineering should be a blend of creativity, logic, and problem-solving
+		skills, and that's exactly what I aim to demonstrate through this website. Here, I will showcase
+		my latest projects and share the process, techniques, and tools used to bring them to life.
+	</p>
+	<p>
+		So whether you're a software engineer yourself or simply interested in learning more about the
+		field, I hope you'll find this site informative and entertaining. Feel free to take a look
+		around and let me know what you think!
+	</p>
+</div>
