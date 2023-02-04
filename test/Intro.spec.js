67eda98afb6edaf63052990840998786ec02d422
Kamal Daniels <kamal.daniels@nanoporetech.com>
2023-02-04 11:41:09 +0000

    Remove old code

--- test/Intro.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Links from '@/components/Links.vue'
-
-describe('Links component', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Links)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
